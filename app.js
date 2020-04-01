ef27a4b92b01b5e0d976c1d3c9407629f81dfd74
Valentin Silvera <valentinsilvera@icloud.com>
2020-04-01 22:40:48 +0200

    Added images and updated layout

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("mySidenav").style.width = "100vw";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
